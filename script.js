e5c297911d05dac33b42b5efe43765bd591a706e
Alfredo Antonio <121048343+alfredoantoni0@users.noreply.github.com>
2023-01-17 11:42:30 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+$(".whack1").click(function(){
+    $(".mole1").hide();
+});
+
+$(".mole1").click(function(){
+    $(".mole1").hide();
+});
+   
+$(".whack2").click(function(){
+    $(".mole-two").hide();
+});
+   
+$(".whack3").click(function(){
+    $(".third-mole").hide();
+});
+   
+$(".whack3").click(function(){
+    $(".mole1").show();
+});
+
+$(".whack3").click(function(){
+    $(".mole-two").show();
+});
+
+$(".reset").click(function(){
+    $("img").show();
+});
+
+$(".yard1").click(function(){
+    $(".game").hide();
+});
